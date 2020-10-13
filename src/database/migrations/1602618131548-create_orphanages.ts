f6eb936e62379f9f14611c915f1ec86bd4d2c523
tiagoramosvasconcelos.work@gmail.com <tiagodev@protonmail.com>
2020-10-13 17:17:04 -0300

    criando e configurando a tabela de banco de dados

--- src/database/migrations/1602618131548-create_orphanages.ts
+++ src/database/migrations/1602618131548-create_orphanages.ts
@@ -1,14 +1,57 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class createOrphanages1602618131548 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         // REALIZA ALTERAÇÕES
         // CRIAR TABELA, CRIAR UM NOVO CAMPO, DELETAR ALGUM CAMPO
+        await queryRunner.createTable(new Table({
+           name: 'orphanages',
+           columns: [
+               {
+                   name: 'id',
+                   type: 'integer',
+                   unsigned: true,
+                   isPrimary: true,
+                   isGenerated: true,
+                   generationStrategy: 'increment',
+               },
+               {
+                   name: 'name',
+                   type: 'varchar',
+               },
+               {
+                   name: 'latitude',
+                   type: 'decimal',
+                   scale: 10,
+                   precision: 2,
+               },
+               {
+                name: 'longitude',
+                type: 'decimal',
+                scale: 10,
+                precision: 2,
+               },
+               {
+                   name: 'about',
+                   type: 'text',
+               },
+               {
+                name: 'instructions',
+                type: 'text',
+               },
+               {
+                   name: 'open_on_weekends',
+                   type: 'boolean',
+                   default: false,
+               },
+           ], 
+        }))
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         // DESFAZER O QUE FOI FEITO DO UP
+        await queryRunner.dropTable('orphanages')
     }
 
 }
