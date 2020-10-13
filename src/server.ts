5da3674b8410272aff1e2ea6b9719705aadbbdd0
tiagoramosvasconcelos.work@gmail.com <tiagodev@protonmail.com>
2020-10-13 16:23:17 -0300

    criando backend nlw#3

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import express from 'express'
+
+const app = express()
+
+app.use(express.json()) // p/ o express entender que é um json
+
+// Rota = conjunto
+// Recurso = usuário
+
+// Método HTTP = GET, POST, PUT, DELETE
+// Parâmetros
+
+// GET = Buscar uma informação (Lista, item)
+// POST = Criando uma informação
+
+// PUT = Editando uma informação
+// DELETE = Deletando uma informação
+
+// Query Params: http://localhost:3333/users?search=tiago
+// Route Params: http:localhost:3333/users/1 (Identificar um recurso)
+// Body: http:localhost:3333/users/1 (Identificar um recurso)
+
+app.get('/users', (request, response) => {
+    return response.json({ message: 'Hello World' })
+})
+
+app.listen(3333)
